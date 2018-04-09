4adf97e2fdd3e2c623fbae40a74c612dcf440e4d
ramadhan07 <35648242+ramadhan07@users.noreply.github.com>
2018-04-09 21:16:34 +0700

    buat getter dan atribut

--- src/praktek003/baju.java
+++ src/praktek003/baju.java
@@ -14,5 +14,37 @@ public class baju {
                 "warna \t: "+warna+"\n"+
                 "harga \t: "+harga);
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getUkuran() {
+        return ukuran;
+    }
+
+    public void setUkuran(String ukuran) {
+        this.ukuran = ukuran;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
     
 }
