7306ff6ac6c574ee8787e6a09f1a0320a6d3c6e7
ramadhan07 <35648242+ramadhan07@users.noreply.github.com>
2018-04-09 21:27:22 +0700

    meggunakan method get

--- src/praktek003/bajuaksi.java
+++ src/praktek003/bajuaksi.java
@@ -11,5 +11,14 @@ public class bajuaksi {
         b.setHarga(5000);
         
         b.cetakinfo();
+        
+        System.out.print("merknya \t: ");
+        System.out.println(b.getMerk());
+        System.out.print("ukurannya \t: ");
+        System.out.println(b.getUkuran());
+        System.out.print("warnanya \t: ");
+        System.out.println(b.getWarna());
+        System.out.print("harganya \t: ");
+        System.out.println(b.getHarga());
     }
 }
