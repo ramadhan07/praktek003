020b1b8ac992f7fe2746aa70f18c74ab6a36a13b
ramadhan07 <35648242+ramadhan07@users.noreply.github.com>
2018-04-09 21:35:14 +0700

    atribute dan method

--- src/praktek003/celanajeans.java
+++ src/praktek003/celanajeans.java
@@ -3,5 +3,10 @@ package praktek003;
 
 
 public class celanajeans extends baju{
+    private String merkcelana;
+    
+    void throttle(){
+        System.out.println("BELIIIIII........");
+    }
     
 }
