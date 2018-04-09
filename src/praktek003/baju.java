ae824ebff6e63d69a0f016380f2dde5904a636d3
ramadhan07 <35648242+ramadhan07@users.noreply.github.com>
2018-04-09 20:56:37 +0700

    membuat atribut

--- src/praktek003/baju.java
+++ src/praktek003/baju.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package praktek003;
 
-/**
- *
- * @author rama
- */
+
 public class baju {
+    String merk;
+    Integer ukuran;
+    String warna;
+    double harga;
     
 }
